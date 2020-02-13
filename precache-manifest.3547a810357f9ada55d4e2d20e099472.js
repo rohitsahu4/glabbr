90cd1755c72ce3ca6b2bbd87aac3e393c678ecc1
Rohit sahu <rohitsahu.4998@gmail.com>
2020-02-14 00:46:52 +0530

    init

---
+++ precache-manifest.3547a810357f9ada55d4e2d20e099472.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c623fbe2618315128253f19daf5ba5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd08f9d73f0390dea6e0",
+    "url": "/static/css/main.474fa854.chunk.css"
+  },
+  {
+    "revision": "ab6ea8301225a99851d4",
+    "url": "/static/js/2.b2f84329.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.b2f84329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd08f9d73f0390dea6e0",
+    "url": "/static/js/main.2ba9ec22.chunk.js"
+  },
+  {
+    "revision": "acacbd7e54a1b2abfecb",
+    "url": "/static/js/runtime-main.f376f8c3.js"
+  },
+  {
+    "revision": "268e89298b9317286fce66edec9efcea",
+    "url": "/static/media/Segoe UI Bold Italic.268e8929.woff"
+  },
+  {
+    "revision": "52382539737f4e9913e4bf6b9966bee3",
+    "url": "/static/media/Segoe UI Bold.52382539.woff"
+  },
+  {
+    "revision": "347c3ac06f20b6a969fc55e89b37c9c4",
+    "url": "/static/media/Segoe UI Italic.347c3ac0.woff"
+  },
+  {
+    "revision": "9a2931180d6b1dc7b33052657eef554b",
+    "url": "/static/media/Segoe UI.9a293118.woff"
+  },
+  {
+    "revision": "3a3dd4201997134be1eb9329c240c363",
+    "url": "/static/media/futura-pt-book-589a6dec272c3.3a3dd420.woff"
+  }
+]);
